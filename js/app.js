6fe6493f9e7666f9b172002524d852bcd8ab69e6
smitul <smitul2310@gmail.com>
2017-04-22 21:42:47 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-
+// Whole-script strict mode syntax
 "use strict";
-
-var gameState = "NotStarted"; 
-var score = 0; 
-var lives = 3; 
-var level = 0; 
-var gemscount = 0; 
-var playerimg = 'images/char-cat-girl.png'; 
-
+// Initialize Global variables
+var gameState = "NotStarted"; //Defines state of the game.
+var score = 0; // define score of the game
+var lives = 3; // plyaers default lives
+var level = 0; // game level
+var gemscount = 0; // gems counter
+var playerimg = 'images/char-cat-girl.png'; //default player image
+// Initialize tile size constants
 var TILE_WIDTH = 101;
 var TILE_HEIGHT = 83;
-
+// Enemies our player must avoid
 var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -43,17 +43,28 @@ Enemy.prototype.render = function() {
 };
 // Now write your own player class
 var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our players, this uses
+    // a helper we've provided to easily load images
     this.sprite = playerimg;
-    this.x = 2 + 120 * (Math.floor(Math.random() * 4)); 
+    this.x = 0; //randomize x position
     this.y = 350;
     this.speed = 10;
 };
-
+// Update the players's position, required method for game
+// Parameter: dt, a time delta between ticks
 Player.prototype.update = function(dt) {
 	this.checkCollisions();
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
     this.x * (dt);
     this.y * (dt);
-    
+    // this.speed * (dt);
+
+    // Reset player when it reaches the water
     if (this.y <= 0) {
         this.reset();
         level = level + 1;
@@ -70,13 +81,14 @@ Player.prototype.update = function(dt) {
 
 
 };
+// Draw the player on the screen, required method for game
 Player.prototype.render = function() {
 	this.renderScoreBoard();
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
+// Check the Collisions wiht Enemies and Gems
 Player.prototype.checkCollisions = function() {
-
+        //check collision with enemies
 		var len=allEnemies.length;
         for (var i = 0; i < len;  i++) {
             if (this.x < allEnemies[i].x + 50 && this.x + 50 > allEnemies[i].x &&
@@ -88,16 +100,19 @@ Player.prototype.checkCollisions = function() {
         }
 
 }
+// Reset the player when it loses life or reaches goals.
 Player.prototype.reset = function() {
     this.x = 100;
     this.y = 400;
 };
+// Player moves to water when colliding with princess
 Player.prototype.water = function() {
     this.x = 100;
     this.y = 0;
 
 
 };
+// Player loses life
 Player.prototype.lives = function() {
     lives = lives - 1;
 	if (lives < 1) {
@@ -105,7 +120,9 @@ Player.prototype.lives = function() {
 		gameState = "GameOver";
 	}
 };
+// This Function render the score on the canvs
 Player.prototype.renderScoreBoard = function() {
+    // Display Score Board with score, level, lives and game label and initial values
     ctx.fillStyle = '#525c65';
     ctx.fillRect( 0, 0, 505,50);
     ctx.strokeStyle="#FFFFFF";
@@ -122,10 +139,11 @@ Player.prototype.renderScoreBoard = function() {
     ctx.strokeText(gemscount, 460, 30);
     ctx.save();
 };
+// This function is for moving the playe
 Player.prototype.handleInput = function(key) {
-    if (key === 'left' && this.x > 101) {
+    if (key === 'left' && this.x > 100) {
         this.x -= TILE_WIDTH;
-    } else if (key === 'right' && this.x < 395) {
+    } else if (key === 'right' && this.x < 404) {
         this.x += TILE_WIDTH;
     } else if (key === 'up' && this.y > 0) {
         this.y -= TILE_HEIGHT;
